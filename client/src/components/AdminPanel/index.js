9db40c7004494f2970847773f0e100cff74c8bd0
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-06-09 13:46:31 -0400

    now when admin deleted inventory, first modal will appear to confirm deleting

--- client/src/components/AdminPanel/index.js
+++ client/src/components/AdminPanel/index.js
@@ -1,6 +1,6 @@
 
-import React, { Component } from "react";
-import { Card, ListGroup, ListGroupItem, Button, Image, CardDeck } from 'react-bootstrap';
+import React, { Component, useState } from "react";
+import { Card, ListGroup, ListGroupItem, Button, Image, CardDeck, Modal } from 'react-bootstrap';
 import App from '../../App'
 import AddItem from '../AddItem'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -13,49 +13,70 @@ class AdminPanel extends Component {
         this.state = {
             isLoggedIn: this.props.isLoggedIn,
             editSession: false,
-            posts:[],
+            posts: [],
             updatePosts: false,
             itemDeleted: false,
             itemPosted: null,
-            pageRefresh: false
+            pageRefresh: false,
+            show: false,
+            setShow: false
         };
-        this.handleDelete = this.handleDelete.bind(this);
+        // this.handleDelete = this.handleDelete.bind(this);
     }
 
-    usePlaceholderImg(ev){
+    usePlaceholderImg(ev) {
         ev.target.src = 'https://upload.wikimedia.org/wikipedia/commons/1/15/No_image_available_600_x_450.svg'
         console.log(ev);
-      }
+    }
 
     addItem = (event) => {
-       this.setState({
-            editSession:true
+        this.setState({
+            editSession: true
         })
     }
-   refreshFeed = (event) => {
-       this.setState({
-           pageRefresh: true
-       }, console.log(this.state.pageRefresh))
+    refreshFeed = (event) => {
+        this.setState({
+            pageRefresh: true
+        }, console.log(this.state.pageRefresh))
+    }
+
+    handleShow = (name,id) => {
+        console.log("Modal for:", name)
+        console.log("id to delete:", id)
+        this.setState({
+            show: true,
+            setShow: true,
+            showDialogue: name,
+            showId: id
+        })
+    }
+    handleClose = () => {
+        this.setState({
+            show: false,
+            setShow: false
+        })
     }
 
 
     fetchPosts() {
         fetch(`/api/posts`)
-        .then(res => res.json())
-        .then(json => {
-          console.log("json", json)
-          this.setState({
-              posts:json.data
-          })
-        })
+            .then(res => res.json())
+            .then(json => {
+                console.log("json", json)
+                this.setState({
+                    posts: json.data
+                })
+            })
     }
+
+ 
 
     handleDelete = id => {
         let item_id = id
         console.log("deleting", item_id)
 
-       const deleteItem = () =>{
-           console.log("posting to DB")
+        const deleteItem = () => {
+            console.log("posting to DB")
             // POST TO DB
             fetch('/api/remove_post', {
                 method: 'DELETE',
@@ -68,36 +89,55 @@ class AdminPanel extends Component {
                 })
             })
             this.setState({
-                itemDeleted:true
+                itemDeleted: true,
+                show: false
             })
         }
         deleteItem();
         this.fetchPosts()
     }
 
-componentDidMount () {
+    componentDidMount() {
 
-    this.fetchPosts();
-    
+        this.fetchPosts();
+
     }
 
 
     render() {
+
         console.log(this.state.posts)
-        const { editSession } = this.state;
+        const { editSession, show, showDialogue, showId} = this.state;
         const items = this.state.posts.map((item, i) =>
-        <Card key={i} className= 'card'>
-            <span onClick={() => this.handleDelete(item.id)} className="delete-icon"><FontAwesomeIcon icon={faTrash} /></span>
-            <p className="text-center">Product Name: {item.product_name}</p>
-       <img className="gun-img" alt={`${item.itemdesc1}`}
-        src={`https://cdsinventoryimages.s3.amazonaws.com/${item.image}`}
-        onError={this.usePlaceholderImg}
-        />
-         <p className="text-center">Item Description: {item.product_description}</p>
-         <p className="text-center">MSRP Price: {item.msrp_price}</p>
-         <p className="text-center">Sale Price: {item.sale_price}</p>
-         
-        </Card>
+            <Card key={i} className='card'>
+                {/* <span onClick={() => this.handleDelete(item.id)} className="delete-icon"><FontAwesomeIcon icon={faTrash} /></span> */}
+                <span onClick={() => this.handleShow(item.product_name, item.id)}  className="delete-icon"><FontAwesomeIcon icon={faTrash} /></span>
+                <p className="text-center">Product Name: {item.product_name}</p>
+                <img className="gun-img" alt={`${item.itemdesc1}`}
+                    src={`https://cdsinventoryimages.s3.amazonaws.com/${item.image}`}
+                    onError={this.usePlaceholderImg}
+                />
+                <p className="text-center">Item Description: {item.product_description}</p>
+                <p className="text-center">MSRP Price: {item.msrp_price}</p>
+                <p className="text-center">Sale Price: {item.sale_price}</p>
+                
+                <Modal centered show={show} onHide={this.handleClose}>
+
+                        <Modal.Header closeButton>
+                            <Modal.Title>Delete Item</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>Are you sure you want to delete {showDialogue}?</Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={this.handleClose}>
+                                Cancel
+                            </Button>
+                            <Button variant="danger" onClick={() => this.handleDelete(showId)}>
+                                Delete
+                                </Button>
+                        </Modal.Footer>
+                    </Modal>
+
+            </Card>
         );
 
         const placeholderText = <div>There are no items in inventory</div>
@@ -114,28 +154,28 @@ componentDidMount () {
                     <Button style={{ backgroundColor: '#dd6717' }} variant='dark' onClick={this.addItem}>Add Item</Button>
                     <div className="mt-5">Current Inventory:</div>
                     <div className="mt-3">
-                    {placeholderText}
-                    </div>                
+                        {placeholderText}
+                    </div>
                 </div>
             )
         }
 
         if (!editSession) {
-         
+
             return (
 
                 <div className="text-center m-5">
-                   
+
                     <Button style={{ backgroundColor: '#dd6717' }} variant='dark' onClick={this.addItem}>Add Item</Button>
                     {/* <i onClick={this.refreshFeed} style={{display: 'block'}} className="mt-3">refresh</i> */}
                     <div className="mt-5">Current Inventory:</div>
                     <CardDeck>
-                     {items}
+                        {items}
                     </CardDeck>
 
                 </div>
             )
-        } 
+        }
     }
 }
 export default AdminPanel

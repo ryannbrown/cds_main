c5c9045b4999448c390c1e6a2b499c6bf422bad0
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-06-18 17:57:41 -0400

    added lmt and aero cards on homepage

---
+++ client/src/components/Cards/index.js
@@ -0,0 +1,33 @@
+import React from "react"
+import {Col, Container, Row} from "react-bootstrap"
+import './style.css'
+// import { Link } from "gatsby"
+/**
+ * Change the content to add your own bio
+ */
+
+
+class Cards extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+
+render() {
+    const { name, id, link } = this.props;
+    return (
+        
+        <div className="card-container mv4">
+    <Container>
+        <Row><Col><h1 className="card-title">We proudly supply the following brands</h1></Col></Row>
+  <Row>
+    <Col><a href="/aeroprecision"><div className="ap-img mt3"><h1 className="card-text">Aero Precision</h1></div></a></Col>
+    <Col><a href="/lmt"><div className="lmt-img mt3"><h1 className="card-text">Lewis Machine & Tool</h1></div></a></Col>
+  </Row>
+       </Container>
+    </div>
+    )
+}
+}
+
+export default Cards
